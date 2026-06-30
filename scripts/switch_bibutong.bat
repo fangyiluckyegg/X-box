1c8c86f33df984d41a2a7341c5aa44a0d497c3ab
fangyiluckyegg <luckyegg@139.com>
2026-07-01 00:22:39 +0800

    家-202607010022

--- scripts/switch_bibutong.bat
+++ scripts/switch_bibutong.bat
@@ -11,6 +11,7 @@ docker compose -f docker-compose.business-bibutong.yml --env-file .env.dev up -d
 :: docker compose --env-file .env.dev -f docker-compose.base.yml -f docker-compose.business-bibutong.yml up -d --build
 :: docker compose --env-file .env.dev -f docker-compose.base.yml -f docker-compose.business-bibutong.dev.yml up -d --build
 :: docker compose --env-file .env.dev -f docker-compose.base.yml -f docker-compose.business-bibutong.dev.yml up -d --build bibutong-backend
+:: docker compose -f docker-compose.base.yml -f docker-compose.business-bibutong.dev.yml up -d --build
 
 :: docker ps
 :: docker ps
