cb1007228f7c8ef1ed942e39a076947f8f8bd219
fangyiluckyegg <luckyegg@139.com>
2026-07-04 16:18:01 +0800

    家-更新202607041617

--- web/prj-frontend/src/main.js
+++ web/prj-frontend/src/main.js
@@ -1,19 +1,13 @@
 import Vue from 'vue'
-
-
 import Element from 'element-ui'
 import './assets/styles/element-variables.scss'
-
 import '@/assets/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
 import directive from './directive' // directive
 import plugins from './plugins' // plugins
-
 import './permission' // permission control
-
-
 import { resetForm } from "@/utils/index";
 // 分页组件
 import Pagination from "@/components/Pagination";
@@ -30,7 +24,6 @@ Vue.use(plugins)
 Vue.use(Element, {
   size: 'medium' // set element-ui default size
 })
-
 
 new Vue({
   el: '#app',
